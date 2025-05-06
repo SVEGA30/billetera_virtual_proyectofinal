f6a8142d6e2564d5ce8dbe25d2038939af282189
SVEGA30 <159830051+SVEGA30@users.noreply.github.com>
2025-05-06 14:31:06 -0500

    javafx.graphics Module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.edu.uniquindio.billeteravirtual {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens co.edu.uniquindio.billeteravirtual to javafx.fxml;
     exports co.edu.uniquindio.billeteravirtual;
